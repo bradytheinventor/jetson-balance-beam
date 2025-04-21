be305b42760d2aec1a461051b54e2e77f77c4cc5
bradytheinventor <bcra1234@gmail.com>
2025-04-21 14:43:59 -0700

    Servo control working

--- i2c_test.py
+++ i2c_test.py
@@ -8,19 +8,34 @@ import time
 bus = smbus.SMBus(1)
 addr = 0x29
 
+mid_us = 1590
+min_us = mid_us-200
+max_us = mid_us+200
+
 def write(msg: str) -> None:
     for c in list(msg):
         bus.write_byte(addr, ord(c))
 
+def write_uint16(i):
+    bytes = [(i&0xFF00) >> 8, i&0x00FF]
+    bus.write_i2c_block_data(addr, bytes[0], bytes[1:])
+
+time.sleep(0.5)
+
 try:
     while(1):
-        write("ON\n")
-        time.sleep(0.05)
-        write("OFF\n")
-        time.sleep(0.05)
+        write_uint16(min_us)
+        time.sleep(1)
+        write_uint16(max_us)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Quitting...")
+    write_uint16(0)
+    time.sleep(0.01)
+
 finally:
     bus.close()
-    print("Quitting...")
 
 print("Done.")
 
